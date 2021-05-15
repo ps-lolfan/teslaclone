a2c14e105362e84d5e1c2943494aee046e841158
ps-lolfan <pbsinnur@gmail.com>
2021-05-15 12:37:18 +0530

    redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,21 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false);
-
+    const cars = useSelector(selectCars);
+    console.log(cars)
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg" alt="" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
-                <a href="#">Solar roof</a>
-                <a href="#">SOlar panels</a>
-            </Menu>
+                {cars && cars.map((car, index) => (<a key={index} href="#">{car}</a>))}
+               </Menu>
             <RightMenu>
                 <a href="#">Shop now</a>
                 <a href="#">Tesla Account</a>
@@ -29,6 +27,9 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick = { () => setBurgerStatus(false)} />
                 </CloseWrapper>
+                {cars && cars.map((car, index) => (
+                <li><a key={index} href="#">{car}</a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in</a></li>
